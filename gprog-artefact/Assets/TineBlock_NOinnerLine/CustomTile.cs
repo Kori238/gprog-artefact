7bfda3a7117c162f7ecd6f62b4f4680064f3f8f5
Kori238 <jakeedwards238@gmail.com>
2023-12-23 22:33:58 +0000

    tile creation and grid setup
    Signed-off-by: Kori238 <jakeedwards238@gmail.com>

---
+++ gprog-artefact/Assets/TineBlock_NOinnerLine/CustomTile.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+[CreateAssetMenu]
+public class CustomTile : Tile
+{
+    public bool layerTraversal = false;
+    public bool walkable = true;
+
+}
